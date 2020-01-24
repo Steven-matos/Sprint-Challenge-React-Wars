e561cd43e30278a59e8ef4454f44a667598c95d9
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-24 12:14:49 -0500

    finalized Card structure with axios calls

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect}  from 'react';
 import axios from 'axios'
 import './App.css';
 import CardContainer from './components/CardContainer/CardContainer';
+import { Container, Row } from 'reactstrap';
+
 
 const App = () => {
   const [character, setCharacter] = useState([]);
@@ -17,23 +19,26 @@ const App = () => {
       })
   }, [])
 
-  console.log(character)
+  console.log("This is the characters info:", character)
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {character.map(data => {
-        return(
-          <CardContainer 
-            key={data.url}
-            name={data.name}
-            birthyear={data.birth_year}
-            homeworld={data.homeworld}
-            species={data.species}
-        />
-        );
-      })}
-      
+      <Container className="themed-container" fluid="sm">
+        <Row xs="4">
+          {character.map(data => {
+            return(
+              <CardContainer 
+                key={data.url}
+                name={data.name}
+                birthyear={data.birth_year}
+                homeworld={data.homeworld}
+                species={data.species}
+            />
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
--- starwars/src/components/CardContainer/CardContainer.js
+++ starwars/src/components/CardContainer/CardContainer.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import {
     Card, CardText, CardBody,
-    CardTitle, CardSubtitle
+    CardTitle, CardSubtitle, Col
 } from 'reactstrap';
+import '../StarWars.css';
+
 
 const CardContainer = (props) => {
+    const [homeworld, setHomeworld] = useState([]);
+    const [species, setSpecies] = useState([]);
+
+    useEffect(() => {
+    axios.get(props.homeworld)
+      .then(res => {
+        setHomeworld(res.data.name)
+      })
+      .catch(err => {
+        console.error("You are getting an error of: ", err);
+      })
+    }, [props.homeworld])
+
+    useEffect(() => {
+        axios.get(props.species)
+            .then(res => {
+                setSpecies(res.data.name)
+            })
+            .catch(err => {
+                console.error("You are getting an error of: ", err);
+            })
+    }, [props.species])
+
     return (
-        <div>
+        <Col>
             <Card>
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardTitle>{props.name}</CardTitle>
+                    <CardSubtitle>{homeworld}</CardSubtitle>
+                    <CardSubtitle>{props.birthyear}</CardSubtitle>
+                    <CardSubtitle>{species}</CardSubtitle>
                     <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                 </CardBody>
             </Card>
-        </div>
+        </Col>
     );
 };
 
